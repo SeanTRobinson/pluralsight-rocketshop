8c60cc07e657b0ce4e55cbf2fbe76fec4ba44d60
Sean Robinson <srobinson@sorensonmedia.com>
2015-12-02 17:12:10 +0000

    Initial commit

---
+++ test/first_spec.js
@@ -0,0 +1,11 @@
+var assert = require('assert');
+
+describe('a feature', function() {
+
+describe('A scenario', function() {
+  it('does something', function() {
+    assert(true);
+  });
+});
+
+});
